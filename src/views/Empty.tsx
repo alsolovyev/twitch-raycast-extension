c51da5ce70b2075cf09295b22818dcb7b089519c
Aleksey Solovyev <solovyev.a@icloud.com>
2022-10-05 21:54:04 +0300

    feat(ui): add Empty view
    A view to display when there aren't any items available.

---
+++ src/views/Empty.tsx
@@ -0,0 +1,19 @@
+import { Color, Icon, Image, List } from '@raycast/api'
+
+
+export interface IEmptyView {
+  title: string
+  description: string
+}
+
+const EmptyIcon: Image.ImageLike = {
+  source: Icon.HeartDisabled,
+  tintColor: Color.Red
+}
+
+/** A view to display when there aren't any items available */
+export default ({ title, description }: IEmptyView) => (
+  <List>
+    <List.EmptyView icon={EmptyIcon} title={title} description={description} />
+  </List>
+)
