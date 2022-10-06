5dc0cae67987e6a3d5bddd65d3c2fcd949edad42
Aleksey Solovyev <solovyev.a@icloud.com>
2022-10-06 13:54:01 +0300

    feat(ui): add a component to display a list of offline streams

---
+++ src/components/OfflineStreams.tsx
@@ -0,0 +1,38 @@
+import { Action, ActionPanel, Icon, List } from '@raycast/api'
+import { ITwitchUser } from '../services/twitch.service'
+
+
+export interface IOfflineStreams {
+  streams: Array<ITwitchUser>
+}
+
+/**
+ * A component to display a list of offline streams.
+ *
+ * @param streams - The list of streams to display.
+ * @returns jsx element.
+ **/
+export default ({ streams }: IOfflineStreams) => (
+  <List.Section
+    title='Offline channels'
+    subtitle={`currently ${streams.length} ${streams.length > 1 ? 'channels' : 'channel'} offline`}
+  >
+    {streams.map(({ description, display_name, id, login }) => (
+      <List.Item
+        icon={{ source: Icon.Livestream }}
+        title={display_name}
+        subtitle={description}
+        key={id}
+        actions={
+          <ActionPanel>
+            <Action.OpenInBrowser
+              icon={Icon.Livestream}
+              title='Open Channel in Browser'
+              url={`https://twitch.tv/${login}`}
+            />
+          </ActionPanel>
+        }
+      />
+    ))}
+  </List.Section>
+)
